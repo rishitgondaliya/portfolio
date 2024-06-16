510d67193721ca7c4ceae73b3c856515f1183381
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-06-16 09:18:25 +0530

    files added

---
+++ script.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 2400, // Animation duration in milliseconds
+    offset: 200,    // Offset (in pixels) from the original trigger point
+    once: false     // Animation should appear both on scroll up and down
+});
