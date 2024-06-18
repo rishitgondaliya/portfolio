f31c0b62e5cebb85499006fcc11b8906ba855367
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-06-18 18:55:19 +0530

    added skills

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 AOS.init({
     duration: 1500, // Animation duration in milliseconds
-    offset: 80,    // Offset (in pixels) from the original trigger point
+    offset: 100,    // Offset (in pixels) from the original trigger point
     once: false     // Animation should appear both on scroll up and down
 });
